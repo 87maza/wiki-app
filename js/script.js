1c75f28e99d8f5a1f1b93d430698b8996a551f50
87maza <87maza@gmail.com>
2016-03-29 22:46:18 -0700

    api exposed in html, needs more styling

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
-//"https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchTerm + "&callback=?&format=json";
-var searchText = document.getElementById('searchText')[0].value;
 
-var submit = document.getElementById("submit");
-submit.addEventListener('click', getResults(searchText));
+$(document).ready(function() {
+	$('#submit').on('click', function() {
+	  $("#title").empty();
+	  var searchText = document.getElementById('searchText').value;
+	  console.log(searchText);
+	  var url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchText + "&callback=?&format=json";
+	  $.getJSON(url, function(response) {
+	  	console.log(response);
+	    var length = response.query.search.length;
+	    var title = document.getElementById('title');
+	    var snippet = document.getElementById('snippet')
 
-function getResults(searchText){
-	// var searchTerm = input.value;
-	console.log(searchText);
-	$.ajax({
-		  url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchText + "&callback=?&format=json",
-		  dataType: "jsonp",
-		  success: function(response) {
-		  	console.log(response);
-		  }
+
+	    for (var i = 0; i < length; i++) {
+	    	title.innerHTML += '<div class="tiles"><a href=\"https://en.wikipedia.org/wiki/' + response.query.search[i].title.replace(/ /g,"_") + "\"" + 'target="_blank"><h2>' + response.query.search[i].title + '</h2>' + '<p>' + response.query.search[i].snippet + '</p></a></div>';	
+	    }
+	  });  
 	});
-}
+    $('#searchText').keypress(function(key){
+    	//pressing enter is the same effect as onclick
+      if(key.keyCode==13)
+      $('#submit').click();
+    });
+});
+
+
+//"https://en.wikipedia.org/w/api.php?action=query&format=json&list=prefixsearch&pssearch=" + input + "&callback=prefixCallback";
